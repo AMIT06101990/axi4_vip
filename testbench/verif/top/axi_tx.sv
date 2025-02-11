6e2a397a6545c7acf8a3a165f6ea0f7c4ed3e28c
AMIT06101990 <ambaliya.amit001@gmail.com>
2025-02-11 19:32:47 +0530

    Add write & read field individually and modify constraint asper write/Read

--- testbench/verif/top/axi_tx.sv
+++ testbench/verif/top/axi_tx.sv
@@ -23,45 +23,81 @@ function new(string name = "axi_tx");
   super.new (name);
 endfunction
 
+//I want to generate write & read on diff address parallaly. 
 //Field Declaration 
-rand bit[7:0]addr;
-rand bit[31:0]dataQ[$];
-rand bit[7:0]wstrbQ[$];
-rand bit wr_rd;
-rand bit[7:0] awid;
-rand bit[7:0] wid;
-rand bit[7:0] bid;
-rand bit[7:0] arid;
-rand bit[7:0] rid;
-rand bit[7:0] burst_len;
-rand bit[2:0] burst_size;
-rand bit resp;
-rand burst_type_t burst_type; 
+//addr_wdith & data_width needs to parameterised 
+rand bit[7:0]              awid;
+rand bit[`ADDR_WIDTH - 1:0]awaddr;
+rand bit[7:0]              awlen;
+rand bit[2:0]              awsize;
+rand burst_type_t          burst_type; 
+
+rand bit[7:0]              wid;
+rand bit[`DATA_WIDTH - 1:0]wdataQ[$];
+rand bit[7:0]              wstrbQ[$];
+
+rand bit[7:0]              bid;
+     bit[1:0]              bresp;
+
+
+rand bit[7:0]              arid;
+rand bit[`ADDR_WIDTH - 1:0]araddr;
+rand bit[7:0]              arlen;
+rand bit[2:0]              arsize;
+
+
+rand bit[7:0]              rid;
+rand bit[`DATA_WIDTH - 1:0]rdataQ[$];
+rand bit                   rresp;
+rand bit                   wr_rd;
 
 //Field registration 
 `uvm_object_utils_begin(axi_tx)
-   `uvm_field_int(addr, UVM_ALL_ON) //Integer/bit field registration macro 
-   `uvm_field_queue_int(dataQ, UVM_ALL_ON) //Queue field registration macro
-   `uvm_field_queue_int(wstrbQ, UVM_ALL_ON)//Queue field registration macro
-   `uvm_field_int(wr_rd, UVM_ALL_ON) //Integer/bit field registration macro
-   `uvm_field_int(awid, UVM_ALL_ON) //Integer/bit field registration macro
-   `uvm_field_int(wid, UVM_ALL_ON) //Integer/bit field registration macro
-   `uvm_field_int(bid, UVM_ALL_ON) //Integer/bit field registration macro
-   `uvm_field_int(arid, UVM_ALL_ON) //Integer/bit field registration macro
-   `uvm_field_int(rid, UVM_ALL_ON) //Integer/bit field registration macro
-   `uvm_field_int(burst_len, UVM_ALL_ON) //Integer/bit field registration macro
-   `uvm_field_int(burst_size, UVM_ALL_ON) //Integer/bit field registration macro
+   `uvm_field_int(awid, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(awaddr, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(awlen, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(awsize, UVM_ALL_ON) //Integer/bit field registration macro 
    `uvm_field_enum(burst_type_t, burst_type, UVM_ALL_ON) //Enum variable registration macro
+   
+   `uvm_field_int(wid, UVM_ALL_ON) //Integer/bit field registration macro
+   `uvm_field_queue_int(wdataQ, UVM_ALL_ON) //Queue field registration macro
+   `uvm_field_queue_int(wstrbQ, UVM_ALL_ON)//Queue field registration macro
+   
+   `uvm_field_int(bid, UVM_ALL_ON) //Integer/bit field registration macro
+   `uvm_field_int(bresp, UVM_ALL_ON) //Integer/bit field registration macro
+  
+   `uvm_field_int(arid, UVM_ALL_ON) //Integer/bit field registration macro
+   `uvm_field_int(araddr, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(arlen, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(arsize, UVM_ALL_ON) //Integer/bit field registration macro 
+  
+  
+   `uvm_field_int(rid, UVM_ALL_ON) //Integer/bit field registration macro
+   `uvm_field_queue_int(rdataQ, UVM_ALL_ON) //Queue field registration macro
+   `uvm_field_int(rresp, UVM_ALL_ON) //Integer/bit field registration macro
+   `uvm_field_int(wr_rd, UVM_ALL_ON) //Integer/bit field registration macro
 `uvm_object_utils_end
+
+//function void post_randomize();
+//  if(!awlen inside {[0:255]} || !arlen inside {[0:255]})
+//    `uvm_fatal("FATAL_ERROR","Constrain for AWLEN and ARLEN not satisfy")
+//endfunction
+
+//function void post_randomize();
+//  if(!awaddr + (2**awsize*(awlen+1)))
+////  if(awaddr % 4096 == (awaddr + (awsize * awlen)) % 4096)
+//    `uvm_fatal("FATAL_ERROR","Constrain For 4KB not satisfy")
+//endfunction
 
 //Constraint for Max Burst legth 
 constraint max_burst_len{
-	burst_len inside {[0:255]};
+	  awlen inside {[0:255]};
+	  arlen inside {[0:255]};
 }
 
 //Constraint for AWID and ARID generation
-constraint id_c{awid inside {[1:15]};
-                arid inside {[1:15]};             
+constraint id_c{awid inside {[1:256]};
+                arid inside {[1:256]};             
 }
   
 //Constraint for Same Write and Read ID 
@@ -72,19 +108,21 @@ constraint awid_wid_c {
 
 //Constraint for Data size & Burst legth
 constraint  burst_len_c{
-  dataQ.size() == burst_len+1;
+  wdataQ.size() == awlen+1;
+  rdataQ.size() == arlen+1;
 }
 
 // Constraint for defaul burst_type & burst_size
 constraint  soft_c{
 	soft burst_type == INCR;
-	soft burst_size == 2;//by default 2**2=4 byte/beat
+	soft awsize == 2;//by default 2**2=4 byte/beat
 
 }
 
 // Constraint for AXI 4KB boundary
 constraint boundary_c{
-	addr + (2**burst_size*(burst_len+1));
+	awaddr + (2**awsize*(awlen+1));
+ //(awaddr / 4096) ==( (awaddr + (awsize * awlen)) / 4096);
 }
 
 
