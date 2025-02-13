6efbd1e30b2bb34bafe1783a331bc094b2f9e592
AMIT06101990 <ambaliya.amit001@gmail.com>
2025-02-13 19:26:41 +0530

    Removed unwanted constraint

--- testbench/verif/top/axi_tx.sv
+++ testbench/verif/top/axi_tx.sv
@@ -1,3 +1,4 @@
+
 //--------------------------------------------------------------------------//
 // Title       : AXI Transaction Class                                      //
 // Class Name  : axi_tx                                                     //
@@ -23,32 +24,47 @@ function new(string name = "axi_tx");
   super.new (name);
 endfunction
 
-//I want to generate write & read on diff address parallaly. 
-//Field Declaration 
-//addr_wdith & data_width needs to parameterised 
+// Write address channel
 rand bit[7:0]              awid;
 rand bit[`ADDR_WIDTH - 1:0]awaddr;
 rand bit[7:0]              awlen;
 rand bit[2:0]              awsize;
-rand burst_type_t          burst_type; 
+rand bit[1:0]              awburst;
+rand bit                   awvalid;
+     bit                   awready;
 
+// Write data channel
 rand bit[7:0]              wid;
 rand bit[`DATA_WIDTH - 1:0]wdataQ[$];
 rand bit[7:0]              wstrbQ[$];
+     bit                   wlast;
+rand bit                   wvalid;
+     bit                   wready;
 
+// Write response channel
 rand bit[7:0]              bid;
      bit[1:0]              bresp;
+     bit                   bvalid;
+rand bit                   bready;
 
-
+// Read address channel
 rand bit[7:0]              arid;
 rand bit[`ADDR_WIDTH - 1:0]araddr;
 rand bit[7:0]              arlen;
 rand bit[2:0]              arsize;
+rand bit[1:0]              arburst;
+rand bit                   arvalid;
+     bit                   arready;
+
+// Read data channel
+     bit[7:0]              rid;
+     bit[`DATA_WIDTH - 1:0]rdataQ[$];
+     bit                   rresp;
+     bit                   rlast;
+     bit                   rvalid;
+rand bit                   rready;
 
 
-rand bit[7:0]              rid;
-rand bit[`DATA_WIDTH - 1:0]rdataQ[$];
-rand bit                   rresp;
 rand bit                   wr_rd;
 
 //Field registration 
@@ -57,25 +73,40 @@ rand bit                   wr_rd;
    `uvm_field_int(awaddr, UVM_ALL_ON) //Integer/bit field registration macro 
    `uvm_field_int(awlen, UVM_ALL_ON) //Integer/bit field registration macro 
    `uvm_field_int(awsize, UVM_ALL_ON) //Integer/bit field registration macro 
-   `uvm_field_enum(burst_type_t, burst_type, UVM_ALL_ON) //Enum variable registration macro
+   `uvm_field_int(awburst, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(awvalid, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(awready, UVM_ALL_ON) //Integer/bit field registration macro 
    
    `uvm_field_int(wid, UVM_ALL_ON) //Integer/bit field registration macro
    `uvm_field_queue_int(wdataQ, UVM_ALL_ON) //Queue field registration macro
    `uvm_field_queue_int(wstrbQ, UVM_ALL_ON)//Queue field registration macro
+   `uvm_field_int(wlast, UVM_ALL_ON) //Integer/bit field registration macro
+   `uvm_field_int(wvalid, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(wready, UVM_ALL_ON) //Integer/bit field registration macro 
    
    `uvm_field_int(bid, UVM_ALL_ON) //Integer/bit field registration macro
    `uvm_field_int(bresp, UVM_ALL_ON) //Integer/bit field registration macro
+   `uvm_field_int(bvalid, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(bready, UVM_ALL_ON) //Integer/bit field registration macro 
   
    `uvm_field_int(arid, UVM_ALL_ON) //Integer/bit field registration macro
    `uvm_field_int(araddr, UVM_ALL_ON) //Integer/bit field registration macro 
    `uvm_field_int(arlen, UVM_ALL_ON) //Integer/bit field registration macro 
    `uvm_field_int(arsize, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(arburst, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(arvalid, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(arready, UVM_ALL_ON) //Integer/bit field registration macro 
   
   
    `uvm_field_int(rid, UVM_ALL_ON) //Integer/bit field registration macro
    `uvm_field_queue_int(rdataQ, UVM_ALL_ON) //Queue field registration macro
    `uvm_field_int(rresp, UVM_ALL_ON) //Integer/bit field registration macro
+   `uvm_field_int(rlast, UVM_ALL_ON) //Integer/bit field registration macro
+   `uvm_field_int(rvalid, UVM_ALL_ON) //Integer/bit field registration macro 
+   `uvm_field_int(rready, UVM_ALL_ON) //Integer/bit field registration macro 
+   
    `uvm_field_int(wr_rd, UVM_ALL_ON) //Integer/bit field registration macro
+
 `uvm_object_utils_end
 
 //function void post_randomize();
@@ -91,33 +122,43 @@ rand bit                   wr_rd;
 
 //Constraint for Max Burst legth 
 constraint max_burst_len{
-	  awlen inside {[0:255]};
-	  arlen inside {[0:255]};
+     awlen inside {[0:255]};
+	   arlen inside {[0:255]};
 }
 
 //Constraint for AWID and ARID generation
-constraint id_c{awid inside {[1:256]};
-                arid inside {[1:256]};             
+constraint id_c{ awid inside {[1:16]};
+                 arid inside {[1:16]};             
 }
-  
-//Constraint for Same Write and Read ID 
-constraint awid_wid_c {
-                awid == wid && wid == bid && awid == bid;
-                arid == rid;
+
+constraint valid_c{
+        soft awvalid==1 && wvalid==1 && arvalid==1;
 }
+    
+ 
+constraint ready_c {
+        soft bready==1 && rready==1;
+}  
+
+constraint data_size_c {
+     soft wdataQ.size() == awlen + 1;
+     soft rdataQ.size() == arlen + 1;
+}  
+ 
+// //Constraint for Same Write and Read ID 
+//constraint awid_wid_c {
+//                awid == wid && wid == bid && awid == bid;
+//                arid == rid;
+//}
 
 //Constraint for Data size & Burst legth
-constraint  burst_len_c{
-  wdataQ.size() == awlen+1;
-  rdataQ.size() == arlen+1;
-}
-
-// Constraint for defaul burst_type & burst_size
-constraint  soft_c{
-	soft burst_type == INCR;
-	soft awsize == 2;//by default 2**2=4 byte/beat
-
-}
+//constraint  soft_c{
+//	soft arburst == 1;
+//	soft awburst == 1;
+//	soft awsize == 2;//by default 2**2=4 byte/beat
+//	soft arsize == 2;//by default 2**2=4 byte/beat
+//
+//}
 
 // Constraint for AXI 4KB boundary
 constraint boundary_c{
